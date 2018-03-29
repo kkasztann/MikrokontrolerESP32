e9cc244fc225dd9227f8f22dcb9775443cedf18c
Kasztan <kkasztann@gmail.com>
2018-03-29 19:23:07 +0200

    Added blink lvl in Blink project

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -9,22 +9,25 @@ void setup()
 
 void loop()
 {
-  if (wyraz!="mrygaj mi tu do rana"){
-    Serial.println("Napisz => mrygaj mi tu do rana");
-    delay(1000);
-
     if(Serial.available())
           {
             wyraz=Serial.readStringUntil('\n');
             Serial.println(wyraz);
           }
- }else{
-   digitalWrite(5, HIGH);
-   delay(100);
-   digitalWrite(5, LOW);
-   delay(100);
- }
+   if (wyraz=="szybko"){
+     digitalWrite(5, HIGH);
+     delay(50);
+     digitalWrite(5, LOW);
+     delay(50);
+   }
 
-
-
+  else if (wyraz=="wolno"){
+    digitalWrite(5, HIGH);
+    delay(1000);
+    digitalWrite(5, LOW);
+    delay(1000);
+  }
+  else if (wyraz=="koniec"){
+    digitalWrite(5, LOW);
+  }
 }
