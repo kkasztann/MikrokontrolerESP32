7987d5138295f27ef6b159ba1a29c96990c42fc5
Kasztan <kkasztann@gmail.com>
2018-04-04 14:36:43 +0200

    added memorizing keys

--- Keyboard/src/main.cpp
+++ Keyboard/src/main.cpp
@@ -14,6 +14,9 @@ char keys[ROWS][COLS] = { //mapowanie klawiatury
   {'*','0','#','D'}
 };
 
+String wpisano=" ";
+bool dopisano=false;
+
 Keypad klawiatura = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //inicjalizacja klawiatury
 
 void setup(){
@@ -24,6 +27,14 @@ void loop(){
   char klawisz = klawiatura.getKey();
 
   if (klawisz){
+    Serial.print("Wciśnięto: ");
     Serial.println(klawisz);
+    dopisano=true;
+  }
+
+  if (dopisano){
+    wpisano+=klawisz;
+    Serial.println("Dotychczas wpisano:"+wpisano);
+    dopisano=false;
   }
 }
